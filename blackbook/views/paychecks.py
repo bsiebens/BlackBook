aa1cc652951af6456ecc9bc41ada4090f12f273f
Bernard Siebens <bernard@siebens.org>
2021-05-12 07:45:16 +0200

    Started working on the paycheck module

--- blackbook/views/paychecks.py
+++ blackbook/views/paychecks.py
@@ -15,14 +15,19 @@ from ..utilities import set_message_and_redirect
 
 @login_required
 def paychecks(request):
-    paychecks = Paycheck.objects.all().order_by("date").prefetch_related("items").prefetch_related("items__category")
+    paychecks = Paycheck.objects.all().order_by("date").prefetch_related("items").prefetch_related("items__category").order_by("-date")
     currency = get_default_currency(user=request.user)
 
     data = {}
 
     for paycheck in paychecks:
         if paycheck.date.year not in data.keys():
-            data[paycheck.date.year] = {"Gross amount": {}, "Taxable amount": {}, "Net amount": {}, "Sum": [Money(0, currency)] * 12}
+            data[paycheck.date.year] = {
+                "Gross amount": {},
+                "Taxable amount": {},
+                "Net amount": {},
+                "Sum": [[Money(0, currency)] * 12, [Money(0, currency)] * 12],
+            }
 
         for item in paycheck.items.all():
             if item.category.name not in data[paycheck.date.year][item.category.type].keys():
@@ -34,7 +39,17 @@ def paychecks(request):
             data[paycheck.date.year][item.category.type][item.category.name]["amount"][paycheck.date.month - 1] = item.amount
             data[paycheck.date.year][item.category.type][item.category.name]["real_amount"][paycheck.date.month - 1] = item.real_amount
 
-        data[paycheck.date.year]["Sum"][paycheck.date.month - 1] = paycheck.amount
+        data[paycheck.date.year]["Sum"][paycheck.date.month - 1][0] = paycheck.amount
+
+        if paycheck.date.month >= 1:
+            data[paycheck.date.year]["Sum"][paycheck.date.month - 1][1] = (
+                paycheck.amount - data[paycheck.date.year]["Sum"][paycheck.date.month - 2][0]
+            )
+        else:
+            if (paycheck.date.year - 1) in data.keys():
+                data[paycheck.date.year]["Sum"][paycheck.date.month - 1][1] = paycheck.amount - data[paycheck.date.year - 1]["Sum"][11][0]
+            else:
+                data[paycheck.date.year]["Sum"][paycheck.date.month - 1][1] = paycheck.amount - Money(0, currency)
 
     for year in data.keys():
         for key in ["Gross amount", "Taxable amount", "Net amount"]:
@@ -43,11 +58,11 @@ def paychecks(request):
             for category in data[year][key].keys():
                 for i in range(12):
                     if data[year][key][category]["real_amount"][i] != Money(0, currency):
-                        if month_sum[i] == Money(9, currency):
+                        if month_sum[i] == Money(0, currency):
                             month_sum[i] = data[year][key][category]["real_amount"][i]
                         else:
                             month_sum[i] += data[year][key][category]["real_amount"][i]
 
             data[year][key]["Sum"] = month_sum
 
-    return render(request, "blackbook/paychecks/list.html", {"data": data})
+    return render(request, "blackbook/paychecks/list.html", {"data": data, "range": range(12)})
